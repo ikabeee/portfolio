cf9ae0071891b488e858e9ddf830c7b3eab69bfc
ikabeee <carlglz30@gmail.com>
2025-09-05 12:25:56 -0500

    feat: implement social media links using a centralized configuration

---
+++ src/constants/socialMedia.ts
@@ -0,0 +1,44 @@
+import Github from "../components/icons/social-media/Github.astro";
+import Instagram from "../components/icons/social-media/Instagram.astro";
+import Linkedin from "../components/icons/social-media/Linkedin.astro";
+import Tiktok from "../components/icons/social-media/Tiktok.astro";
+import Youtube from "../components/icons/social-media/Youtube.astro";
+import Threads from "../components/icons/social-media/Threads.astro";
+import X from "../components/icons/social-media/X.astro";
+export const socialMedia = [
+    {
+        name: "GitHub",
+        url: "https://github.com/ikabeee",
+        icon: Github,
+    },
+    {
+        name: "Instagram",
+        url: "https://www.instagram.com/ikabeeee/",
+        icon: Instagram,
+    },
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/carlglzm/",
+        icon: Linkedin,
+    },
+    {
+        name: "TikTok",
+        url: "https://www.tiktok.com/@ik4beee",
+        icon: Tiktok,
+    },
+    {
+        name: "YouTube",
+        url: "https://www.youtube.com/@ikabeee",
+        icon: Youtube,
+    },
+    {
+        name: "Threads",
+        url: "https://www.threads.com/@ikabeeee",
+        icon: Threads,
+    },
+    {
+        name: "X",
+        url: "https://x.com/ikabeeee",
+        icon: X,
+    },
+];
