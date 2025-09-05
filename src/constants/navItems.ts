36aa05a1618ffae6edc67c496fd34af8e8004fa1
ikabeee <carlglz30@gmail.com>
2025-09-05 18:36:02 -0500

    feat: add Button component and refactor Navbar to use navItems constant

---
+++ src/constants/navItems.ts
@@ -0,0 +1,7 @@
+export const navItems = [
+    { name: "Acerca de mí", href: "#about" },
+    { name: "Habilidades", href: "#skills" },
+    { name: "Proyectos", href: "#projects" },
+    { name: "Experiencia", href: "#experience" },
+    { name: "Contacto", href: "#contact" },
+]
