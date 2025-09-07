0807eb0c130baaa41d7fc5b27a04ff105b9c083c
ikabeee <carlglz30@gmail.com>
2025-09-07 17:21:31 -0500

    feat: enhance layout and styling of contact, information, hero, and project components; improve responsiveness and accessibility

--- src/constants/socialMedias.ts
+++ src/constants/socialMedias.ts
@@ -1,10 +1,10 @@
 import Github from "../components/icons/social-media/Github.astro";
-import Instagram from "../components/icons/social-media/Instagram.astro";
 import Linkedin from "../components/icons/social-media/Linkedin.astro";
-import Tiktok from "../components/icons/social-media/Tiktok.astro";
-import Youtube from "../components/icons/social-media/Youtube.astro";
-import Threads from "../components/icons/social-media/Threads.astro";
-import X from "../components/icons/social-media/X.astro";
+// import Instagram from "../components/icons/social-media/Instagram.astro";
+// import Tiktok from "../components/icons/social-media/Tiktok.astro";
+// import Youtube from "../components/icons/social-media/Youtube.astro";
+// import Threads from "../components/icons/social-media/Threads.astro";
+// import X from "../components/icons/social-media/X.astro";
 import type { SocialMedia } from "../interfaces/SocialMedia";
 
 
@@ -14,34 +14,34 @@ export const socialMedias: SocialMedia[] = [
         url: "https://github.com/ikabeee",
         icon: Github,
     },
-    {
-        name: "Instagram",
-        url: "https://www.instagram.com/ikabeeee/",
-        icon: Instagram,
-    },
+    // {
+    //     name: "Instagram",
+    //     url: "https://www.instagram.com/ikabeeee/",
+    //     icon: Instagram,
+    // },
     {
         name: "LinkedIn",
         url: "https://www.linkedin.com/in/carlglzm/",
         icon: Linkedin,
     },
-    {
-        name: "TikTok",
-        url: "https://www.tiktok.com/@ik4beee",
-        icon: Tiktok,
-    },
-    {
-        name: "YouTube",
-        url: "https://www.youtube.com/@ikabeee",
-        icon: Youtube,
-    },
-    {
-        name: "Threads",
-        url: "https://www.threads.com/@ikabeeee",
-        icon: Threads,
-    },
-    {
-        name: "X",
-        url: "https://x.com/ikabeeee",
-        icon: X,
-    },
+    // {
+    //     name: "TikTok",
+    //     url: "https://www.tiktok.com/@ik4beee",
+    //     icon: Tiktok,
+    // },
+    // {
+    //     name: "YouTube",
+    //     url: "https://www.youtube.com/@ikabeee",
+    //     icon: Youtube,
+    // },
+    // {
+    //     name: "Threads",
+    //     url: "https://www.threads.com/@ikabeeee",
+    //     icon: Threads,
+    // },
+    // {
+    //     name: "X",
+    //     url: "https://x.com/ikabeeee",
+    //     icon: X,
+    // },
 ];
