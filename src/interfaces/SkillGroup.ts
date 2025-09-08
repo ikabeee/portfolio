5304deaaa3c455d1d95c78d55dd0dc05322eb17a
Carlos Gonzalez <108252279+ikabeee@users.noreply.github.com>
2025-09-08 00:22:01 -0500

    Revert "Develop"

--- src/interfaces/SkillGroup.ts
+++
@@ -1,9 +0,0 @@
-export interface SkillGroup {
-    icon:  astroHTML.JSX.Element;
-    name: string;
-    technologies: {
-        name: string;
-        url: string;
-        icon:  astroHTML.JSX.Element;
-    }[]; 
-}
